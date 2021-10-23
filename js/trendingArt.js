53ff398eca1b7d2a18bedf56266abe5e3b3cb1fc
Priyanshi-Raj <priyanshiraj2909@gmail.com>
2021-10-23 15:03:30 +0530

    trending and nav added

--- js/trendingArt.js
+++ js/trendingArt.js
@@ -3,45 +3,45 @@ const trending = [
         "id" : "51",
         "subject" : "Chemistry",
         "name" : "Juliet",
-        "topic" : "Physical",
+        "profile" : "./images/Ellipse 1 (1)",
         "about" : "Environment",
-        "img" : "./assets/alumna.jpg",
+        "img" : "./images/Rectangle 7 (4).png",
        
     },
     {
         "id" : "52",
         "subject" : "English",
         "name" : "Bill Gates",
-        "topic" : "Leaders Speak",
+        "profile" : "Leaders Speak",
         "about" : "Environment",
-        "img" : "./assets/2021-10-09 (2).png",
+        "img" : "./images/Rectangle 7 (4).png",
        
     },
     {
         "id" : "52",
         "subject" : "English",
         "name" : "Bill Gates",
-        "topic" : "Leaders Speak",
+        "profile" : "Leaders Speak",
         "about" : "Environment",
-        "img" : "./assets/sdfsd 1.png",
+        "img" : "./images/Rectangle 7 (4).png",
         
     },
     {
         "id" : "52",
         "subject" : "English",
         "name" : "Romeo Juliet",
-        "topic" : "Leaders Speak",
+        "profile" : "Leaders Speak",
         "about" : "Environment",
-        "img" : "./assets/college-student.jpg",
+        "img" : "./images/Rectangle 7 (4).png",
        
     },
     {
         "id" : "52",
         "subject" : "English",
         "name" : "Juliet",
-        "topic" : "Leaders Speak",
+        "profile" : "Leaders Speak",
         "about" : "Environment",
-        "img" :"./assets/alumna.jpg",
+        "img" :"./images/Rectangle 7 (4).png",
        
     }
     
@@ -53,13 +53,15 @@ for(let j =0;j<trending.length;j++)
     let createCard = document.createElement('div');
              createCard.innerHTML= `<div class="card bg-light m-3 card-style" style="width: 14rem; height:24.5rem; border-radius:20px;">
         
-          <img style="border-radius: 20px;" class="trending-img" width="100%" height="100%" src="${trending[j].img}" alt="">
+          <img style="border-radius: 20px;" class="trending-img" width="95%" height="70%" style="border-radius:20px;" src="${trending[j].img}" alt="">
              <div class="card-body trending-body"  style = "background: rgba(255,255,255,0.55);
              color: #000000;width:100%;">
              <h5 class="card-title makeBold">${trending[j].name}</h5>
+             <div>
+             <img style="border-radius: 20px;" class="trending-img" width="95%" height="70%" style="border-radius:20px;" src="${trending[j].profile}" alt="">
              <p class="card-text">
              SmartEd
-             </p>
+             </p></div>
              </div>
              </div>`
              document.getElementById('card-add').appendChild(createCard);
